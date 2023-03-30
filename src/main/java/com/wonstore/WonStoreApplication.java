984b8bf71ab2f0d8ea1e8b882440c2c6b0aecc42
jeongjiwon <jungkt0x@naver.com>
2023-03-30 21:54:45 +0900

    initial commit

---
+++ src/main/java/com/wonstore/WonStoreApplication.java
@@ -0,0 +1,13 @@
+package com.wonstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonstore/WonStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
