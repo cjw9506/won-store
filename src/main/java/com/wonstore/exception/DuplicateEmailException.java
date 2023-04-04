7dd0768371e55fc67839583e0071f581cd56cc6e
jeongjiwon <jungkt0x@naver.com>
2023-04-05 00:38:15 +0900

    add Exception

--- src/main/java/com/wonstore/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.wonstore.exception;
-
-public class DuplicateEmailException extends Exception {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/wonstore/exception/DuplicateIdException.java
+++
@@ -1,7 +0,0 @@
-package com.wonstore.exception;
-
-public class DuplicateIdException extends Exception{
-    public DuplicateIdException(String message) {
-        super(message);
-    }
-}
